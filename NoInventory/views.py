9752cc15f03b00ffcc92557d48763cec93bb923b
hugobarzano <hugobarzano@gmail.com>
2016-02-22 17:22:50 +0100

    codigos qr

--- NoInventory/views.py
+++ NoInventory/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 import time
 from django.http import HttpResponse
 from NoInventory.forms import *
+from django.http import HttpResponseRedirect
 
 from pymongo import MongoClient
 client = MongoClient('mongodb://localhost:27017/')
@@ -27,7 +28,11 @@ def nuevoItem(request):
                         "tipo_item": form.data['tipo_item'],
                         "estado_item": form.data['estado_item'],
                         }
-            items.insert(item)
+            id_item=items.insert(item)
+            #print id_item
+            qr_data_generated=jsonTOstring(items.find_one({"_id": id_item}))
+            #print qr_data_generated
+            items.update_one({"_id":id_item},{"$set": {"qr_data": qr_data_generated}})
             lista_items=db.items.find()
             #print lista_items
             #for i in lista_items:
@@ -39,3 +44,8 @@ def nuevoItem(request):
     else:
         form = ItemForm()
     return render(request, 'noinventory/nuevoItem.html', {'form': form})
+
+def jsonTOstring(elemento):
+    #d=json.dumps(elemento)
+    texto="Nombre Item:" + elemento["nombre_item"] + "\nIdentificador:"+str(elemento["_id"]) + "\nFecha de Alta:"+elemento["fecha_alta_item"]+"\nDescripcion:"+elemento["descripcion_item"]+"\nEstado:"+elemento["estado_item"]+"\nTipo:"+elemento["tipo_item"]+"\nTags:"+elemento["tag_item"]
+    return texto
