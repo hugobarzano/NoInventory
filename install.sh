6274af8bb6f1de0e60642c9bc064ce20460a3428
hugobarzano <hugobarzano@gmail.com>
2016-03-04 18:56:12 +0100

    instalador

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Actualizar Sistema Base
+echo "actualizar sistema base"
+sudo apt-get -y update
+
+# Instalar Python y PostgreSQL
+echo "Instalar dependencias"
+sudo apt-get -y install git
+sudo apt-get -y install python-setuptools
+sudo apt-get -y install python-dev
+sudo apt-get -y install build-essential
+sudo apt-get -y install libpq-dev
+sudo apt-get -y install mongodb
+sudo easy_install pip
+sudo pip install --upgrade pip
+sudo python -m pip install pymongo==3.1.1
+#Instalamos requisitos
+echo "instalar requirements"
+sudo pip install -r requirements.txt
---
+++ makefile
@@ -0,0 +1,4 @@
+#Makefile
+install:
+	sudo chmod +x install.sh
+	sudo ./install.sh
