9c404bd01cba8c521d2452ac0b3771728cbae494
hugobarzano <hugobarzano@gmail.com>
2016-03-14 11:14:09 +0100

    procfile!

--- NoInventory/test_item.py
+++ NoInventory/test_item.py
@@ -1,4 +1,5 @@
-from bson.objectid import ObjectId
+#from bson.objectid import ObjectId
+from pymongo.objectid import ObjectId
 from pymongo import MongoClient
 import time
 from item import *
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn NoInventoryProject.wsgi --log-file -
